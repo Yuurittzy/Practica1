e4d4398560f57de460e7d8eb00990085326c47a2
Yu <yu@Yus-MacBook-Pro.local>
2022-02-25 00:25:42 -0600

    aparecer o no

--- app/src/main/java/com/example/practica1_m4_yuritzy/MainActivity.kt
+++ app/src/main/java/com/example/practica1_m4_yuritzy/MainActivity.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.content.res.AppCompatResources
+import com.example.practica1_m4_yuritzy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setSpinnerData()
     }
 
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 showImage(position)
+                showText(position)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -39,11 +41,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun goToResultActivity(view: View) {
-        startActivity(Intent(this, ResultActivity::class.java))
+        if (binding.etX.text.toString()=="")
+        {
+            Toast.makeText(this,"Por favor ingresa un número",Toast.LENGTH_SHORT).show()
+        } else{
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
     }
 
     fun showImage(position: Int) {
-        val image = findViewById<ImageView>(R.id.imageView3)
+        val image = findViewById<ImageView>(R.id.ivFormula)
         val resource = when(position) {
             0 -> AppCompatResources.getDrawable(this, R.drawable.formula_gral)
             1 -> AppCompatResources.getDrawable(this, R.drawable.potencia)
@@ -51,9 +59,32 @@ class MainActivity : AppCompatActivity() {
 
             else -> AppCompatResources.getDrawable(this, R.drawable.formula_gral)
         }
-
-
         image.setImageDrawable(resource)
     }
 
+    fun showText(position: Int) {
+        val tvx= findViewById<TextView>(R.id.tvX)
+        val tvy= findViewById<TextView>(R.id.tvY)
+        val tvz= findViewById<TextView>(R.id.tvZ)
+        val et= findViewById<EditText>(R.id.etZ)
+
+        if (position==1){
+            tvz.visibility = View.INVISIBLE
+            et.visibility = View.INVISIBLE
+            tvx.text = "W"
+            tvy.text = "t"
+        }else if(position==2){
+            tvz.visibility = View.INVISIBLE
+            et.visibility = View.INVISIBLE
+            tvx.text = "b"
+            tvy.text = "h"
+        }
+        else{
+            tvz.visibility = View.VISIBLE
+            tvx.text = "a"
+            tvy.text = "b"
+            tvz.text = "c"
+        }
+
+    }
 }
